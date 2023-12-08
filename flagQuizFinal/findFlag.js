f2e20aae7cb8b1fd2af4b18a4690fce5ac541964
jackspoon <jackson.graybeal@gmail.com>
2023-12-07 19:19:56 -0700

    FinalyWorkingFlagFinal

--- flagQuizFinal/findFlag.js
+++ flagQuizFinal/findFlag.js
@@ -4,6 +4,8 @@ const option = document.getElementById("flagFinds")
 
 const Flagthing = document.getElementsByClassName("Flagthing")
 
+const flagButton = document.getElementById("flagSubmit")
+
 for (let index = 0; index < guesses.length; index++) {
     const element = guesses[index];
     element.addEventListener("focus", dropBar)
@@ -20,6 +22,56 @@ function select() {
     option.classList.add("hidden")
     currentGuess.value = this.innerHTML 
     blur()
+}
+
+flagButton.addEventListener("click", submit) 
+
+function submit() {
+    if (whatsThatFlag.value == "") {
+        alert("Please fill out all fields")
+        return
+    }
+    const allFlags = document.getElementsByClassName("flag")
+    for (let index = 0; index < allFlags.length; index++) {
+        const element = allFlags[index];
+        element.classList.add("hidden")
+    }
+    switch (whatsThatFlag.value) {
+        case document.getElementById("flag1").innerHTML: 
+            document.getElementById("bgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag2").innerHTML:
+            document.getElementById("cgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag3").innerHTML:
+            document.getElementById("germgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag4").innerHTML:
+            document.getElementById("ggrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag5").innerHTML:
+            document.getElementById("italygrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag6").innerHTML:
+            document.getElementById("Japan").classList.remove("hidden")
+            break;
+        case document.getElementById("flag7").innerHTML:
+            document.getElementById("swgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag8").innerHTML:
+            document.getElementById("sgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag9").innerHTML:
+            document.getElementById("tgrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag10").innerHTML:
+            document.getElementById("ugrid").classList.remove("hidden")
+            break;
+        case document.getElementById("flag11").innerHTML:
+            document.getElementById("agrid").classList.remove("hidden")
+        default:
+            break;
+    }
 }
 
 function dropBar() {
